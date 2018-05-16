120eb4a895029e4822868a4da4642d38f21b8ecd
siambro <muntasirhasan32@gmail.com>
2018-05-17 04:35:05 +0600

    first

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use DB;
+use Session;
+session_start();
+class LoginController extends Controller
+{
+    //
+    public function index(){
+        return view('Manager.index');
+    }
+    public function login_manager(Request $request){
+        // return view('Manager/index');
+        $userName = $request -> user_name;
+        $pass = md5($request -> pass);
+        $result = DB::table('users')
+        ->where('userName', $userName)
+        ->where('password', $pass)
+        ->first(); 
+
+        if($result){
+            Session::put('userName',$result->userName);
+            Session::put('phone',$result->phone);
+            Session::put('id',$result->id);
+            return Redirect::to('/index');
+        }else{
+            Session::put('exception','Invalid User name or password');
+            return Redirect::to('/');
+        }
+
+        // print_r($result);
+    }
+}
